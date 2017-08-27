c61c63f13961e4953e81f5acbb195f74c4bc6c70
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2017-08-27 19:31:56 +0800

    Plots and Tensorboard removed

--- linear-regression-model/linear-regression.py
+++ linear-regression-model/linear-regression.py
@@ -11,7 +11,8 @@ linear_model = tf.add(tf.multiply(m, x), c)
 y = tf.placeholder(tf.float32, name='y')
 
 # Loss
-loss = tf.reduce_sum(tf.square(tf.subtract(linear_model, y)))  # sum of the squares
+# sum of the squares
+loss = tf.reduce_sum(tf.square(tf.subtract(linear_model, y)))
 # Optimizer
 optimizer = tf.train.GradientDescentOptimizer(0.01, name='train_min')
 train = optimizer.minimize(loss)
@@ -21,27 +22,12 @@ x_train = [1, 2, 3, 4]
 y_train = [0, -2, -1, 10]
 # Training loop
 init = tf.global_variables_initializer()
-# Run the session and produce Tensorboard graph
-# tensorboard --logdir="./graphs"
-with tf.Session() as sess:
-    # Tensorboard
-    writer = tf.summary.FileWriter('./graphs', sess.graph)
-    # Reset values
-    sess.run(init)
-    # Train
-    for i in range(1000):
-        sess.run(train, {x: x_train, y: y_train})
-    # Evaluate training accuracy
-    curr_m, curr_c, curr_loss = sess.run([m, c, loss], {x: x_train, y: y_train})
-    print("m: %s c: %s loss: %s" % (curr_m, curr_c, curr_loss))
-# Close writer
-writer.close()
-
-# Plot
-X = np.array(range(0,7))  
-Y = eval('curr_m*X + curr_c')
-plt.scatter(x_train, y_train, color='red')
-plt.plot(X, Y)
-plt.xlabel('x')
-plt.ylabel('y')
-plt.show()
+# Reset values
+sess = tf.Session()
+sess.run(init)
+# Train
+for i in range(1000):
+    sess.run(train, {x: x_train, y: y_train})
+# Evaluate training accuracy
+curr_m, curr_c, curr_loss = sess.run([m, c, loss], {x: x_train, y: y_train})
+print("m: %s c: %s loss: %s" % (curr_m, curr_c, curr_loss))
