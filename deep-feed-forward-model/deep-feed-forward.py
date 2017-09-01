0463bd024d04371e37b3091846cc4d0dbc5c435f
Nikhil Raghavendra <raghavendranikhil@gmail.com>
2017-09-01 11:41:10 +0800

    README.md added

--- deep-feed-forward-model/deep-feed-forward.py
+++ deep-feed-forward-model/deep-feed-forward.py
@@ -5,9 +5,9 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
 # Hidden layers
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
+n_nodes_hl1 = 800
+n_nodes_hl2 = 800
+n_nodes_hl3 = 800
 
 # Classes
 n_classes = 10
@@ -19,12 +19,12 @@ batch_size = 100
 x = tf.placeholder(tf.float32, [None, 784])
 y = tf.placeholder(tf.float32)
 
-# Deep neural network model
+
 def neural_network_model(data):
     # (input_data * weights) + biases
     hidden_1_layer = {'weights': tf.Variable(tf.random_normal([784, n_nodes_hl1])),
                       'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
-    
+
     hidden_2_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
                       'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
@@ -32,7 +32,7 @@ def neural_network_model(data):
                       'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
     output_layer = {'weights': tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                      'biases': tf.Variable(tf.random_normal([n_classes]))}
+                    'biases': tf.Variable(tf.random_normal([n_classes]))}
 
     # Model
     l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
@@ -56,8 +56,8 @@ def train_neural_network(x):
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=prediction))
     # Optimizer (Adam)
     optimizer = tf.train.AdamOptimizer().minimize(cost)
-    # Model will train for 100 cycles, feed forward + backprop
-    epochs = 100
+    # Model will train for 10 cycles, feed forward + backprop
+    epochs = 10
 
     # Session
     with tf.Session() as sess:
@@ -66,7 +66,7 @@ def train_neural_network(x):
         # Run cycles
         for epoch in range(epochs):
             epoch_loss = 0
-            for _ in range(int(mnist.train.num_examples/batch_size)):
+            for _ in range(int(mnist.train.num_examples / batch_size)):
                 epoch_x, epoch_y = mnist.train.next_batch(batch_size)
                 _, c = sess.run([optimizer, cost], feed_dict={x: epoch_x, y: epoch_y})
                 epoch_loss += c
